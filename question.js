449ed6a3cb6698f3bd93544445d509df79b0dc0a
Rachel Wong <hw4wong@edu.uwaterloo.ca>
2019-03-15 22:27:30 -0400

    initial commit

---
+++ question.js
@@ -0,0 +1,68 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_US: [
+    {
+      'Did you eat?': [
+        'Yes',
+        'No',
+        'Sort of',
+      ],
+    },
+    {
+      'Did you work enough? ': [
+        'Yes I did all of my tasks',
+        'No I was lazy',
+        'I did half',
+      ],
+    },
+    {
+      'Are you stressed today?': [
+        'Not much',
+        'A lot',
+        'I lost track',
+      ],
+    },
+    {
+      'Did you distress? ': [
+        'Yes',
+        'what is that',
+        'no i dont know how',
+        'No way to tell',
+      ],
+    },
+    {
+      'Did you make yourself happy?': [
+        'Yes',
+        'Maybe',
+        'What is happy',
+        'Life sucks',
+      ],
+    },
+    {
+      'Do you forgive others?': [
+        'Yes sometimes we should let things slide',
+        'No why will you do that',
+        'I am just angry and pissed',
+      ],
+    },
+    {
+      'Do you make mistakes and forgive yourself?': [
+        'Everyone makes mistakes doesnt mean that you are not good',
+        'But I still did something bad',
+        'Some mistakes are impossible to forgive',
+      ],
+    },
+    {
+      'Are you content with life?': [
+        'Yes',
+        'No My life is far from perfect',
+        'Life is so bad',
+      ],
+    },
+  ],
+};
